22998cc455ef6507a8f93af640f916b63275aff0
Aravind Reddy V <zerodividedby0@gmail.com>
2017-09-06 16:35:07 +0530

    First and Second

---
+++ LAB 3/12.m
@@ -0,0 +1,38 @@
+function [array] = randomArray (arrayLength)
+  array = rand(arrayLength, 1);
+  for i = 1:length(array)
+    if (array(i) < 0.5)
+      array(i) = -1;
+    else
+      array(i) = 1;
+    end
+  end
+end
+
+
+function [time, signal] = getU (arrayLength, dt)
+  temp_signal = randomArray(arrayLength);
+  
+  signal = ones(arrayLength, 1/dt);
+  signal = bsxfun(@times, signal, temp_signal(:));
+  
+  signal = signal';
+  signal = signal(:);
+  
+  time = 0:dt:arrayLength - dt;
+end
+
+arrayLength = 10;
+dt = 0.001;
+
+[time1, Uc] = getU (arrayLength, dt);
+[time2, Us] = getU (arrayLength, dt);
+
+carrier = arrayfun(@(t) cos(40*pi*t), time1);
+
+up = Uc.*carrier;
+
+plot(time1 , Uc);
+
+xlabel("Time");
+ylabel("Magnitude");
